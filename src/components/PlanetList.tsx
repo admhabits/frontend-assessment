64cf257315ab54ba5d19218da11dbbc5d9eb7372
Alam Wibowo <alamhafidz61@gmail.com>
2023-09-26 18:46:14 +0700

    Fixed Margin Gap Between Card

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -3,61 +3,64 @@ import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface Planet {
-  name: string;
+    name: string;
 }
 
 const PlanetList: FC = () => {
-  const [planets, setPlanets] = useState<Planet[]>([]);
-  const [hasMore, setHasMore] = useState<boolean>(true);
+    const [planets, setPlanets] = useState<Planet[]>([]);
+    const [hasMore, setHasMore] = useState<boolean>(true);
 
-  const fetchPlanets = async () => {
-    try {
-      const response = await axios.get('https://swapi.dev/api/planets');
-      setPlanets(planets.concat(response.data.results));
-    } catch (error) {
-      console.error('Error fetching planets:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPlanets();
-  }, []);
-
-  const loadMore = () => {
-    fetchPlanets();
-  };
-
-  return (
-    <div>
-      <InfiniteScroll
-        dataLength={planets.length}
-        next={loadMore}
-        hasMore={hasMore}
-        loader={
-          <h4
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-            }}
-          >
-            Loading...
-          </h4>
+    const fetchPlanets = async () => {
+        try {
+            const response = await axios.get('https://swapi.dev/api/planets');
+            setPlanets(planets.concat(response.data.results));
+        } catch (error) {
+            console.error('Error fetching planets:', error);
         }
-        height={'90vh'}
-      >
-        {planets.map((planet, index) => (
-          <div
-            key={index}
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4 border rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out"
-          >
-            {planet.name}
-          </div>
-        ))}
-      </InfiniteScroll>
-    </div>
-  );
+    };
+
+    useEffect(() => {
+        fetchPlanets();
+    }, []);
+
+    const loadMore = () => {
+        fetchPlanets();
+    };
+
+    return (
+        <div>
+            <InfiniteScroll
+                dataLength={planets.length}
+                next={loadMore}
+                hasMore={hasMore}
+                loader={
+                    <h4
+                        style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)',
+                        }}
+                    >
+                        Loading...
+                    </h4>
+                }
+                height={'90vh'}
+            >
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
+                    {planets.map((planet, index) => (
+                        <div
+                            key={index}
+                            className="gap-4 p-4 m-2 border rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out"
+                        >
+                            {planet.name}
+                        </div>
+                    ))}
+                </div>
+
+            </InfiniteScroll>
+        </div>
+    );
 };
 
 export default PlanetList;
